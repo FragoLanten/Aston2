0f6363fbe6b27dd77d29f32c1d9175ae6d450454
Vadim <rizinvadim@gmail.com>
2023-07-30 17:58:33 +0300

    second commit

--- src/main/java/rest/controller/MainController.java
+++ src/main/java/rest/controller/MainController.java
@@ -1,12 +1,13 @@
 package rest.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String sayHello() {
         return "hello";
     }
